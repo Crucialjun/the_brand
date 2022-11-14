ac0b236a6070dbccc7235dce7be629dfe37455cd
Crucialjun <nich.otieno@gmail.com>
2022-11-14 17:30:16 +0300

    initial

---
+++ lib/features/utils/global_constants.dart
@@ -0,0 +1,2 @@
+const String jsonDataStreamUrl =
+    "https://jsoneditoronline.org/#right=local.qiqiva&left=cloud.bc1d1f888aee4171b3adf29a87923bb8https://jsoneditoronline.org/#right=local.qiqiva&left=cloud.bc1d1f888aee4171b3adf29a87923bb8";
